ce05a8e902c8133a1b01afb23d5a4826827cc8df
RicardoAlonzo <alonzoricardo6@gmail.com>
2018-10-09 17:27:28 +0200

    Front User Signup, Login, Logout ready

---
+++ front/src/components/navbar/Navbar.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import AuthService from '../auth/AuthService';
+
+class Navbar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { loggedInUser: null };
+        this.service = new AuthService();
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.setState({ ...this.state, loggedInUser: nextProps["userInSession"] })
+    }
+
+    handleLogout = (e) => {
+        this.props.logout()
+    }
+
+    render() {
+        if (this.state.loggedInUser) {
+            return (
+
+
+                <nav className="navbar navbar-dark bg-dark">
+                    <h2>Welcome, {this.state.loggedInUser.username}</h2>
+                    <a onClick={this.handleLogout}>Logout</a>
+                </nav>
+            )
+        } else {
+            return (
+                <div>
+                    <nav className="navbar navbar-dark bg-dark">
+                        <h2>BitWalla</h2>
+                        <div className='NavbarNewGhest'>
+                            <Link to='/signup'>Signup</Link>
+                            <Link to='/login'>Login</Link>
+                        </div>
+                    </nav>
+                </div>
+            )
+        }
+    }
+}
+
+export default Navbar;
